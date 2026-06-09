d189883d3c4da72aa1bf0119f7250f05d1df5fa0
Vinns <kamaukevin0033@gmail.com>
2026-06-09 12:07:37 +0000

    Restore blank lines per gofumpt requirements

--- apps/api/internal/fcm/notifier.go
+++ apps/api/internal/fcm/notifier.go
@@ -9,6 +9,7 @@ import (
 
 	"firebase.google.com/go/v4/messaging"
 )
+
 var (
 	// ErrUnavailable indicates FCM service is temporarily unavailable.
 	ErrUnavailable = errors.New("fcm: temporarily unavailable")
