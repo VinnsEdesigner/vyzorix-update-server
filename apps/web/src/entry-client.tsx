51f27753bfa6e1e0f5a1f536dca2638c26d79a34
Vinns <kamaukevin0033@gmail.com>
2026-06-12 00:04:41 +0000

    fix: fix import order in entry-client.tsx

--- apps/web/src/entry-client.tsx
+++ apps/web/src/entry-client.tsx
@@ -1,9 +1,9 @@
 // @vyzorix/web - Client-side entry point for SSR hydration
 // This file handles client-side hydration of server-rendered HTML
 
+import { StartClient } from "@tanstack/react-start/client";
 import { startTransition } from "react";
 import { hydrateRoot } from "react-dom/client";
-import { StartClient } from "@tanstack/react-start/client";
 
 // Hydrate the application
 startTransition(() => {
