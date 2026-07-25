538c5b01b7c6d10d3630a26555371e59b5b734d6
Vinns <kamaukevin0033@gmail.com>
2026-07-25 14:45:53 +0000

    fix(api-client): align membership mutations with server schema
    - Use inline fields instead of imported fragment
    - Add SUSPEND_MEMBER and REINSTATE_MEMBER mutations

--- packages/API_Client/src/vyzorServer/graphql/membership/graphql-membership-mutations.ts
+++ packages/API_Client/src/vyzorServer/graphql/membership/graphql-membership-mutations.ts
@@ -1,11 +1,26 @@
 import { graphqlClient } from '../_shared/graphql-client';
-import { MEMBERSHIP_FRAGMENT } from './graphql-membership-types';
 
 export const UPDATE_MEMBER_ROLE = `
-  ${MEMBERSHIP_FRAGMENT}
   mutation UpdateMemberRole($organizationId: ID!, $memberId: ID!, $role: OrgRole!) {
     updateMemberRole(organizationId: $organizationId, memberId: $memberId, role: $role) {
-      ...MembershipFields
+      id
+      organizationId
+      operatorId
+      role
+      lifecycle
+      invitedAt
+      joinedAt
+      removedAt
+      suspendedAt
+      operator {
+        id
+        email
+        name
+      }
+      organization {
+        id
+        name
+      }
     }
   }
 `;
@@ -13,6 +28,18 @@ export const UPDATE_MEMBER_ROLE = `
 export const REMOVE_MEMBER = `
   mutation RemoveMember($organizationId: ID!, $memberId: ID!) {
     removeMember(organizationId: $organizationId, memberId: $memberId)
+  }
+`;
+
+export const SUSPEND_MEMBER = `
+  mutation SuspendMember($organizationId: ID!, $memberId: ID!) {
+    suspendMember(organizationId: $organizationId, memberId: $memberId)
+  }
+`;
+
+export const REINSTATE_MEMBER = `
+  mutation ReinstateMember($organizationId: ID!, $memberId: ID!) {
+    reinstateMember(organizationId: $organizationId, memberId: $memberId)
   }
 `;
 
@@ -26,6 +53,20 @@ export async function mutateUpdateMemberRole(params: { organizationId: string; m
 export async function mutateRemoveMember(params: { organizationId: string; memberId: string }) {
   return graphqlClient.getClient().mutate({
     mutation: REMOVE_MEMBER,
+    variables: params,
+  });
+}
+
+export async function mutateSuspendMember(params: { organizationId: string; memberId: string }) {
+  return graphqlClient.getClient().mutate({
+    mutation: SUSPEND_MEMBER,
+    variables: params,
+  });
+}
+
+export async function mutateReinstateMember(params: { organizationId: string; memberId: string }) {
+  return graphqlClient.getClient().mutate({
+    mutation: REINSTATE_MEMBER,
     variables: params,
   });
 }
