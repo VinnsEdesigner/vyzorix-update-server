cfd84125b3422884a336e06a07ce753c0b271015
Vinns <kamaukevin0033@gmail.com>
2026-07-25 14:25:22 +0000

    fix(api-client): align invitation queries with server schema
    Invitation queries:
    - Change from 'me { invitations }' to 'myInvitations' query
    - Change from 'organization { invitations }' to 'organizationInvitations' query
    - Add pagination to organizationInvitations
    - Fix query function exports

--- packages/API_Client/src/vyzorServer/graphql/invitation/graphql-invitation-queries.ts
+++ packages/API_Client/src/vyzorServer/graphql/invitation/graphql-invitation-queries.ts
@@ -1,24 +1,26 @@
-
-
-import { gql } from '@apollo/client';
+import { graphqlClient } from '../_shared/graphql-client';
 import { INVITATION_FRAGMENT } from './graphql-invitation-types';
 
-export const GET_MY_INVITATIONS = gql`
+export const GET_MY_INVITATIONS = `
   query GetMyInvitations {
-    me {
-      invitations {
-        ...InvitationFields
-      }
+    myInvitations {
+      ...InvitationFields
     }
   }
   ${INVITATION_FRAGMENT}
 `;
 
-export const GET_ORGANIZATION_INVITATIONS = gql`
-  query GetOrganizationInvitations($organizationId: ID!) {
-    organization(id: $organizationId) {
-      invitations {
+export const GET_ORGANIZATION_INVITATIONS = `
+  query GetOrganizationInvitations($organizationId: ID!, $page: Int, $limit: Int) {
+    organizationInvitations(organizationId: $organizationId, page: $page, limit: $limit) {
+      items {
         ...InvitationFields
+      }
+      pagination {
+        page
+        limit
+        total
+        totalPages
       }
     }
   }
@@ -27,9 +29,32 @@ export const GET_ORGANIZATION_INVITATIONS = gql`
 
 export const GET_INVITATION_BY_TOKEN = gql`
   query GetInvitationByToken($token: String!) {
-    invitation(token: $token) {
+    invitationByToken(token: $token) {
       ...InvitationFields
     }
   }
   ${INVITATION_FRAGMENT}
 `;
+
+export async function queryMyInvitations() {
+  return graphqlClient.getClient().query({
+    query: GET_MY_INVITATIONS,
+    fetchPolicy: 'network-only',
+  });
+}
+
+export async function queryOrganizationInvitations(params: { organizationId: string; page?: number; limit?: number }) {
+  return graphqlClient.getClient().query({
+    query: GET_ORGANIZATION_INVITATIONS,
+    variables: params,
+    fetchPolicy: 'network-only',
+  });
+}
+
+export async function queryInvitationByToken(token: string) {
+  return graphqlClient.getClient().query({
+    query: GET_INVITATION_BY_TOKEN,
+    variables: { token },
+    fetchPolicy: 'network-only',
+  });
+}
